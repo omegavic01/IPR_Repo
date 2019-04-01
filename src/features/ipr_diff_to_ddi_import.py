cc55f52117b77d9d42f22707a3bc5ad6e93015ca
omegavic01 <44787170+omegavic01@users.noreply.github.com>
2019-04-01 13:08:17 -0500

    Beggining update

--- src/features/ipr_diff_to_ddi_import.py
+++ src/features/ipr_diff_to_ddi_import.py
@@ -747,7 +747,7 @@ def main():
 
     # Build File and File path.
     src_file = os.path.join(processed_data_path,
-                            'Potential Updates for DDI.xlsx')
+                            'ADD Agency.xlsx')
     ea_data_file = os.path.join(raw_data_path, 'ea_data.pkl')
     ddi_data_file = os.path.join(raw_data_path, 'ddi_data.pkl')
     add_file = os.path.join(reports_data_path, 'Add Import.csv')
